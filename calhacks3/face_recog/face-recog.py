c8ac5d1428c1582cb80d7429bc16f3c40d48161a
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2024-10-19 22:10:13 -0500

    emotion detect

--- calhacks3/face_recog/face-recog.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from deepface import DeepFace
-from os import getenv
-
-print(cv2.__file__)
-cv2.__file__ = getenv("OPENCV_DATA_PATH", None)
-print(cv2.__file__)
-
-DeepFace.stream(db_path = "./face-recog/photodatabase", model_name="Facenet512", enable_face_analysis=True, time_threshold=3, frame_threshold=3)
